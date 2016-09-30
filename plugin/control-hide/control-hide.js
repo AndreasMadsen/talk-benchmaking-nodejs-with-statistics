deb503f271ae2f33b7138a5210ce97573720b35d
Andreas Madsen <amwebdk@gmail.com>
2016-09-30 11:38:20 +0200

    [plugin] toggle controls class

--- plugin/control-hide/control-hide.js
+++ plugin/control-hide/control-hide.js
@@ -8,6 +8,8 @@
 
         Reveal.configure({ controls: !controls });
 
+        document.documentElement.classList.toggle("controls", !controls);
+
         const videos = document.getElementsByTagName('video');
         for (var i = 0; i < videos.length; i++) {
           videos[i].controls = !controls;
